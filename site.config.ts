0400207e59445aac8a2054cbb5e8478505216f38
leanbiz <leanbizvn@gmail.com>
2023-02-21 05:38:38 +0000

    init project

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'LeanBiz-VN-a85a29ad62cb4d7fac270e9f7bde4eab',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
